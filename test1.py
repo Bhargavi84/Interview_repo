c54b48f20c29ae012da202cb0bf36f0edf73177f
Bhargavi84 <bhargavi.parikshit@yahoo.com>
2022-04-11 08:58:57 +0000

    Adding Question1 and Question2 codes

---
+++ test1.py
@@ -0,0 +1,8 @@
+N = input("Enter any number between 1 and 5: ")
+S = input("Enter the number in words: (ex.1 as one): ")
+num_dict = {"one": 1, "two": 2, "three": 3, "four": 4, "five": 5}
+
+z = int(N)*num_dict[S]
+print("The product of " + N + " and " +S + " is: ", z)
+
+           
---
+++ test2.py
@@ -0,0 +1,7 @@
+N = input("Enter anynumber between 0 and 50: ")
+S = input("Choose a day from the list - Mon,Tue,Wed,Thur,Fri,Sat,Sun: ")
+days_list = ["Mon", "Tue", "Wed", "Thur", "Fri", "Sat", "Sun"]
+K = days_list.index(S)
+i = K + int(N)%7
+print(N+" days after " + S + " is: "+ days_list[i])
+      
