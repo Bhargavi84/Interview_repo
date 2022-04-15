107a3faed32342269fba7291883d5caaadf8127c
Bhargavi84 <bhargavi.parikshit@yahoo.com>
2022-04-15 07:43:23 +0000

    Adding function to code

--- question1.py
+++ question1.py
@@ -1,6 +1,11 @@
+def get_product(N, S):
+    num_dict = {"one": 1, "two": 2, "three": 3, "four": 4, "five": 5}
+    z = int(N)*num_dict[S]
+    return z
+    
 N = input("Enter any number between 1 and 5: ")
 S = input("Enter the number in words: (ex.1 as one): ")
-num_dict = {"one": 1, "two": 2, "three": 3, "four": 4, "five": 5}
 
-z = int(N)*num_dict[S]
-print("The product of " + N + " and " +S + " is: ", z)
+
+product = get_product(N, S)
+print("The product of " + N + " and " +S + " is: ", product)
--- question2.py
+++ question2.py
@@ -1,6 +1,11 @@
+def get_day(N, K):
+    i = (K + int(N))%7
+    return i
+
 N = input("Enter anynumber between 0 and 50: ")
 S = input("Choose a day from the list - Mon,Tue,Wed,Thur,Fri,Sat,Sun: ")
 days_list = ["Mon", "Tue", "Wed", "Thur", "Fri", "Sat", "Sun"]
 K = days_list.index(S)
-i = K + int(N)%7
-print(N+" days after " + S + " is: "+ days_list[i])
+
+day_num = get_day(N, K)
+print(N+" days after " + S + " is: "+ days_list[day_num])
